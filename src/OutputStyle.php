1ffe29e07b32fd29f1f81da31d11596b1fc868a9
Oliver Stark <os@fortrabbit.com>
2018-04-26 13:35:59 +0200

    OutputStyle::type() added: Emulate terminal typing

--- src/OutputStyle.php
+++ src/OutputStyle.php
@@ -7,4 +7,34 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class OutputStyle extends SymfonyStyle
 {
 
+    /**
+     * Emulate terminal typing
+     *
+     * @param string $command
+     * @param string $style
+     * @param int    $speed
+     * @param string|null $prepend
+     */
+    public function type(string $command, string $style = "fg=white", $speed = 5, string $prepend = "$ ")
+    {
+        if (!is_null($prepend)) {
+            $this->write($prepend);
+        }
+
+        $chars = str_split($command);
+
+        foreach ($chars as $char) {
+
+            $this->write(sprintf("<%s>%s</>", $style, $char));
+
+            if (is_int($speed) && $speed > 0) {
+                $delay = rand(10000, 1000000) / $speed;
+                usleep($delay);
+            }
+        }
+
+        // delay before new line
+        usleep(1000000 / $speed);
+        $this->newLine();
+    }
 }
